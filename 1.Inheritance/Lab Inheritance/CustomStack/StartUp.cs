4f2e811e7e4decd95bbe2fb5f53fc4fd540e761c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-04 21:40:12 +0300

    task 5 complete

---
+++ 1.Inheritance/Lab Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            StackOfStrings vs = new StackOfStrings();
+            vs.Push("asd");
+            Console.WriteLine(vs.AddRange());
+            Console.WriteLine(vs.IsEmpty()); 
+        }
+    }
+}
