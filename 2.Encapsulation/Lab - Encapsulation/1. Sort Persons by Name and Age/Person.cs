628aac35c1833e99d990f78fc1128d626e5e1f2d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-07 16:42:07 +0300

    task 2 complete

--- 2.Encapsulation/Lab - Encapsulation/1. Sort Persons by Name and Age/Person.cs
+++ 2.Encapsulation/Lab - Encapsulation/1. Sort Persons by Name and Age/Person.cs
@@ -9,12 +9,14 @@ namespace PersonsInfo
         private string firstName;
         private string lastName;
         private int age;
+        private decimal salary;
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Age = age;
+            this.Salary = salary;
         }
 
         public string FirstName
@@ -50,9 +52,34 @@ namespace PersonsInfo
                 age = value;
             }
         }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+               
+                salary = value;
+            }
+        }
+
+        public decimal IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                this.Salary += this.Salary * percentage/100/2;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            return this.Salary;
+        }
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
         }
     }
 }
