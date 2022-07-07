83fdeb217766947813d20f041be4468f586b9913
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-07 13:08:59 +0300

    task 1 completed

---
+++ 1.Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set 
+            {
+                name = value; 
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set 
+            { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("A person's age canno't be a negative number!");
+                }
+                age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
