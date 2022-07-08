040e68c5c146e63288c0c76cbdabf4722f23b324
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-08 10:45:23 +0300

    task 4 complete

---
+++ 2.Encapsulation - Exercise/Exercise - Encapsulation/ShoppingSpree/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> boughtProducts;
+
+        public Person()
+        {
+            boughtProducts = new List<Product>();
+        }
+        public Person(string name, decimal money):this()
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> BoughtProducts
+        {
+            get
+            {
+                return boughtProducts.AsReadOnly();
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            boughtProducts.Add(product);
+        }
+        public void ReduceMoney(decimal money)
+        {
+            this.Money -= money;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in boughtProducts)
+            {
+                sb.Append(item.Name + ",");
+            }
+            string[] toSplit = sb.ToString().Split(",",StringSplitOptions.RemoveEmptyEntries);
+            string toReturn = string.Join(", ", toSplit);
+            return toReturn.ToString().Trim();
+        }
+
+    }
+}
---
+++ 2.Encapsulation - Exercise/Exercise - Encapsulation/ShoppingSpree/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Product> productsList = new List<Product>();
+            List<Person> personsList = new List<Person>();
+            string[] peopleInput = Console.ReadLine().Split(";",StringSplitOptions.RemoveEmptyEntries);
+            foreach (string person in peopleInput)
+            {
+                try
+                {
+                    string[] personInfo = person.Split("=");                    
+                    string name = personInfo[0];
+                    decimal money = decimal.Parse(personInfo[1]);
+                    Person newPerson = new Person(name,money);
+                    personsList.Add(newPerson);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                    return;
+                }
+            }
+            string[] productsInput = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string product in productsInput)
+            {
+                try
+                {
+                    string[] productInfo = product.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    string name = productInfo[0];
+                    decimal money = decimal.Parse(productInfo[1]);
+                    Product newProduct = new Product(name,money);
+                    productsList.Add(newProduct);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                    return;
+                }
+            }
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person currentPerson = personsList.FirstOrDefault(x => x.Name == cmdArgs[0]);
+                Product currentProduct = productsList.FirstOrDefault(x => x.Name == cmdArgs[1]);
+
+                if(currentPerson.Money >= currentProduct.Cost)
+                {
+                    currentPerson.AddProduct(currentProduct);
+                    currentPerson.ReduceMoney(currentProduct.Cost);
+                    Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                }
+            }
+            foreach (Person person1 in personsList)
+            {
+                if (person1.BoughtProducts.Count > 0)
+                {
+                    Console.WriteLine($"{person1.Name} - {person1.ToString()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person1.Name} - Nothing bought");
+                }
+            }
+        }
+    }
+}
