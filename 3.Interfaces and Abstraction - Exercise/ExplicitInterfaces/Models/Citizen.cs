e6fc5d06001e3b8291f3af31c7892173aed4b665
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-12 14:30:55 +0300

    task 9 complete

---
+++ 3.Interfaces and Abstraction - Exercise/ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,49 @@
+﻿using ExplicitInterfaces.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IResident, IPerson
+    {
+        private string name;
+        private string country;
+        private int age;
+
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            private set { country = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+
+        string IPerson.GetName()
+        {
+            return $"{this.Name}";
+        }
+    }
+}
---
+++ 3.Interfaces and Abstraction - Exercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExplicitInterfaces
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<IPerson> citizensPerson = new List<IPerson>();
+            List<IResident> citizensResident = new List<IResident>();
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = input.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                string name = cmdArgs[0];
+                string country = cmdArgs[1];
+                int age = int.Parse(cmdArgs[2]);
+                Citizen citizen = new Citizen(name, country, age);
+                citizensPerson.Add(citizen);
+                citizensResident.Add(citizen);
+            }
+            for (int i = 0; i < citizensPerson.Count; i++)
+            {
+                IPerson currentPerson = citizensPerson[i];
+                IResident currentResident = citizensResident[i];
+                Console.WriteLine(currentPerson.GetName());
+                Console.WriteLine(currentResident.GetName());
+                
+
+
+            }
+        }
+    }
+}
