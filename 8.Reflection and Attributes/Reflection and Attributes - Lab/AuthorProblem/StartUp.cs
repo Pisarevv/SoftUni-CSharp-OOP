c73b28841c8a286289f92cc1fbe13afb75caf7b3
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-23 16:45:17 +0300

    task 5 complete

---
+++ 8.Reflection and Attributes/Reflection and Attributes - Lab/AuthorProblem/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Hristo")]
+    public class StartUp
+    {
+        [Author("Hristo")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
