f6529b800f58953a14bda6dbdfd741cf2e39a0a6
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-23 16:00:14 +0300

    task 1 complete

---
+++ 8.Reflection and Attributes/Reflection and Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate,params string[] fieldsToInvestigate)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type classType = Type.GetType(classToInvestigate);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {classToInvestigate}");
+
+            foreach (FieldInfo field in classFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
