31412c2829ad7dac81f41604730ef62dafe31ebb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-07-25 22:58:17 +0300

    task 2 complete

--- 9.Unit Testing/Skeleton.Tests/DummyTests.cs
+++ 9.Unit Testing/Skeleton.Tests/DummyTests.cs
@@ -6,9 +6,50 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void Test_DummyLosingHealthAfterAttack()
         {
-
+            //Arrange
+            Dummy dummy = new Dummy(100, 100);
+            //Act 
+            dummy.TakeAttack(5);
+            //Assert 
+            Assert.That(dummy.Health, Is.EqualTo(95));
+        }
+        [Test]
+        public void Test_DummyThrowsExceptionWhenDeadAndAttacked()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(2, 100);
+            //Act and Assert
+            Assert.Multiple(() =>
+            {
+                dummy.TakeAttack(2);
+                Assert.That(() => dummy.TakeAttack(4), Throws.InvalidOperationException, "Dummy is dead.");
+            });
+        }
+        [Test]
+        public void Test_DummyShoudNotGiveExeprienceWhenDead()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(2, 100);
+            //Act and Assert
+            Assert.Multiple(() =>
+            {
+                dummy.TakeAttack(3);
+                Assert.That(() => dummy.GiveExperience().Equals(100));
+            });
+        }
+        [Test]
+        public void Test_AliveDummyShoudNotGiveExperince()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(22, 100);
+            //Act and Assert
+            Assert.Multiple(() =>
+            {
+                dummy.TakeAttack(2);
+                Assert.That(() => dummy.GiveExperience(), Throws.InvalidOperationException, "Target is not dead.");
+            });
         }
     }
 }
